bac6b6a78d0781050beb4a223214839e96a29211
Ruben Wuyts <wuytsruben@gmail.com>
2026-09-12 15:20:59 +0200

    eerste stap persistentie

---
+++ Assessment/Assessment.Core/Entities/DocumentMetaData.cs
@@ -0,0 +1,19 @@
+﻿namespace Assessment.Core.Entities
+{
+    public class DocumentMetadata
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Description { get; set; }
+
+        public DocumentMetadata(string author, string title, DateTime creationDate, string description)
+        {
+             Author = author;
+            Title = title;
+            CreationDate = creationDate;
+            Description = description;
+        }
+
+    }
+}
